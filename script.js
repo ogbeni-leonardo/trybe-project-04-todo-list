b449f5ec3bbc811697f90aa432c030a492dd15d6
Leonardo C Araújo <ogbeni-leonardo@outlook.com>
2022-04-21 00:23:52 -0300

    Resolução dos requisitos 6 e 7

--- script.js
+++ script.js
@@ -2,12 +2,19 @@ const listaTarefas = document.getElementById('lista-tarefas');
 const textoTarefa = document.getElementById('texto-tarefa');
 const criaTarefa = document.getElementById('criar-tarefa');
 
+// Altera a cor de fundo das li's ao clicar sobre elas
+function listBackgroundColor(element) {
+  element.style.backgroundColor = 'gray';
+}
+
 // Adiciona item na lista de tarefas
 function adicionarTarefa() {
   const tarefa = textoTarefa.value;
   if (tarefa.length > 0) {
     const listItem = document.createElement('li');
     listItem.innerText = tarefa;
+    listItem.onclick = () => listBackgroundColor(listItem);
+
     listaTarefas.appendChild(listItem);
     textoTarefa.value = '';
   }
